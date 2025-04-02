56797c164325e204dfe3e4977995e9cf59655796
aditibarua229 <barua0972@mydusd.org>
2025-04-02 12:17:20 -0700

    Create Primate.java

---
+++ Primate.java
@@ -0,0 +1,6 @@
+class Primate extends Animal {
+    public Primate(String food, boolean nocturnal, double aveLifeSpan) {
+        super(food, nocturnal, aveLifeSpan);
+        System.out.println("A feline arrives.");
+    }
+}

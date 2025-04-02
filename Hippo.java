68c6abdfdfac78fdbefaa030cfd9613aedbdf4e3
aditibarua229 <barua0972@mydusd.org>
2025-04-02 12:14:19 -0700

    Create Hippo.java

---
+++ Hippo.java
@@ -0,0 +1,14 @@
+/*
+ * Activity 4.9.2
+ */
+public class Hippo extends Hooved
+{
+  public Hippo() {
+        super("water plants", false, 30.0);
+        System.out.println("A hippo arrives.");
+  }
+  public void groan()
+  {
+    System.out.println("The hippo groans.");
+  }
+}

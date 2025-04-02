d361caa6696731eea0a628e995a5bc9e12ceacea
aditibarua229 <barua0972@mydusd.org>
2025-04-02 12:13:30 -0700

    Create Feline.java

---
+++ Feline.java
@@ -0,0 +1,6 @@
+class Feline extends Animal {
+    public Feline(String food, boolean nocturnal, double aveLifeSpan) {
+        super(food, nocturnal, aveLifeSpan);
+        System.out.println("A feline arrives.");
+    }
+}
